5a963a04794df43da20eb80a4877e10e4c8555e8
ssteuteville <ssteuteville@gmail.com>
2013-10-14 16:32:47 -0700

    Foreground/Daemon run/log modes available
    Implemented 2 new functions and changed a third:
    	log(tag, message) -- this function is used for logging non-error
    	messages.
    
    	logError(tag, message) -- this function is used for logging errors.
    
    	error(tag, e) used to be printError(e). -- this function calls the
    	logError function with parameters based on the value's recieve from
    	the exception object passed in.
    These functions are used in approriate places. I haven't tested this
    very robustly yet. But the service still runs and works as before.
    
    This commit closes #5. I had to change the logging system around a bit
    to compliment both modes we can run our service in.

--- src/qwifi.py
+++ src/qwifi.py
@@ -5,103 +5,88 @@ import time
 import sys
 import syslog
 from daemon import DaemonContext
+from collections import namedtuple
 import subprocess
 
-log = True
+modes = ("SYSLOG","FOREGROUND")#to add a new mode we just need another tuple entry
+#create class modes with attributes that are members of modes tuple
+modes = namedtuple("modes", modes)(*range(len(modes)))
+
+logMode = modes.SYSLOG
+
 def dropConnection(macAddr):
-  #print "Mac Address " + macAddr #for testing
-  syslog.syslog("Mac Address %s is being dropped." %macAddr)
+  log("dropConnection", "Mac Address %s is being dropped." %macAddr)
   subprocess.call(["sudo", "hostapd_cli", "disassociate", macAddr])
 
-def printError(e):
+def error(tag, e):
     try:
-        print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-        syslog.syslog(syslog.LOG_ERR, "Unable to open the database")
+        logError(tag, "MySQL Error [%d]: %s" % (e.args[0], e.args[1]) )
     except IndexError:
-        print "MySQL Error: %s" % str(e)
-        syslog.syslog(syslog.LOG_ERR, "Unable to open the database, perhaps wrong values passed in?")
-
-
+        logError(tag,"MySQL Error: %s" % str(e))
+        
 def updateRadcheck(dataBase, cursor):
     try:
         cursor.execute("INSERT INTO radcheck (username, attribute, op, value) SELECT radcheck.username, 'Auth-Type', ':=', 'Reject' FROM radcheck INNER JOIN radacct ON radcheck.username=radacct.username WHERE radcheck.attribute='Session-Timeout' AND TIMESTAMPDIFF(SECOND, radacct.acctstarttime, NOW()) > radcheck.value AND radacct.acctstoptime is NULL;")
         if int(cursor.rowcount) > 0:
-          syslog.syslog("We have updated something into radcheck")
+            log("updateRadcheck", "we have updated radcheck")
         dataBase.commit()    
-        print "updated radcheck"
     except MySQLdb.Error, e:
-        printError(e)
+        error("updateRadcheck", e)
         dataBase.rollback()
-        print("Error updating radcheck")
-        syslog.syslog(syslog.ERR, "Error updating radcheck table.")
 
 def disassociate(cursor):
     cursor.execute("SELECT radacct.callingstationId FROM radcheck INNER JOIN radacct ON radcheck.username=radacct.username WHERE radcheck.value = 'Reject' AND radacct.acctstoptime is NULL;")
     mac_addresses = set(cursor.fetchall())
     for macAddr in mac_addresses:
+        log("dissassociate", "dropping %s" % macAddr)
         threading.Thread(target=dropConnection(macAddr[0].replace('-', ':')))
 
 def cull(dataBase, cursor):
     try:
-        if log == True:
-          cursor.execute("SELECT username FROM radcheck WHERE value = 'Reject';")
-          users_culled = cursor.fetchall()
-          for user in users_culled:
-            #print "Taking out %s user from radcheck." %user
-            syslog.syslog("Taking out %s user from radcheck." %user)
-
+        cursor.execute("SELECT username FROM radcheck WHERE value = 'Reject';")
+        users_culled = cursor.fetchall()
+        for user in users_culled:
+            log("cull","Taking out %s user from radcheck." %user)
         cursor.execute("DELETE FROM radcheck WHERE username IN (SELECT username FROM (SELECT username FROM radcheck WHERE value='Reject') temp);")
         if int(cursor.rowcount) > 0:
-          syslog.syslog("We have deleted %s number of things from radcheck" %cursor.rowcount)
+            log("cull","We have deleted %s number of things from radcheck" %cursor.rowcount)
         dataBase.commit()
-        print "Cleaned up radcheck"
     except MySQLdb.Error, e:
-        printError(e)
+        error("cull",e)
         dataBase.rollback()
-        print("Error cleaning up radcheck")
-        syslog.syslog(syslog.ERR, "Error cleaning up radcheck")
 
+def log(tag, message):
+    global logMode
+    if logMode == modes.FOREGROUND:
+        print("<" + tag + ">" + message)
+    elif logMode == modes.SYSLOG:
+        syslog.syslog("<" + tag + ">" + message)
+
+def logError(tag, message):
+    global logMode
+    if logMode == modes.FOREGROUND:
+        print("<" + tag + ">" + message)
+    elif logMode == modes.SYSLOG:
+        syslog.syslog(syslog.ERR, "<" + tag + ">" + message)   
 
 def main():  
   while True:
-    print "loop entry"
     try:
         db = MySQLdb.connect("localhost","root","password","radius")
-        #db = MySQLdb.connect("localhost", "test", "test123", "radius")
         cursor = db.cursor()
         syslog.syslog("Started logging process on daemon.")
     except MySQLdb.Error, e:
-        printError(e)
+        error("main", e)
 
     updateRadcheck(db, cursor)#update radcheck with reject for old sessions
     disassociate(cursor)#kick off all of the old sessions
     cull(db, cursor)#remove unneccassery data from DB
     time.sleep(5) #loop every 5 seconds
 
-#the stdout/stderror part is for testing
-with DaemonContext(working_directory = '.', stdout=sys.stdout, stderr=sys.stderr):
-    main()
-
-
-
-
-
-
-
-
-
-###FUTURE ADAPTATIONS###
-#def subsets(l, n):
-    #for i in xrange(0, len(l), n):
-        #yield l[i:i+n]
-
-#if len(macAddr) >= 100:
-#  for subset in subsets(mac_addresses, 100):
-#    threads = []
-#    for macAddr in subset:
-#      t = threading.Thread(target=dropConnection(macAddr))
-#      threads.append(t)
-#    [i.join() for i in threads]#wait for each thread to finish
-#else:
-#  for macAddr in mac_addresses:
-#    threading.Thread(target=dropConnection(macAddr))
+if len(sys.argv) > 1:
+    if sys.argv[1] == "-n":
+        logMode = modes.FOREGROUND
+        main()
+else:
+    with DaemonContext(working_directory = '.'):
+        main()
