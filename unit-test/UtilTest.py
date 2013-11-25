f838ca15f7dc108fe04faab1e80d93818bc4d9cf
ssteuteville <ssteuteville@gmail.com>
2013-11-25 09:55:26 -0800

    added command line parsing unit test

--- unit-test/UtilTest.py
+++ unit-test/UtilTest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import unittest
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
@@ -13,6 +14,18 @@ class UtilTest(unittest.TestCase):
         result = [fl for fl in qwifi.freeloader_gen(test)]
         self.assertEqual(actual_freeloaders, result, "qwifi.freeloader_gen does not generate the correct values.\nGenerated Values:"
                                                 + str(actual_freeloaders) + "\ndoes not equal\nExpected:" + str(result))
+    def test_parse_args(self):
+        sys.argv.append("-n")
+        args = qwifi.parse_args()
+        self.assertEqual("Namespace(c='qwifi.conf', n=True)", str(args), "-n command line arg fail")
+        sys.argv.pop()
+        sys.argv.append("-c")
+        sys.argv.append("test")
+        args = qwifi.parse_args()
+        self.assertEqual("Namespace(c='test', n=False)", str(args), "-c command line arg fail")
+        sys.argv.append("-n")
+        args = qwifi.parse_args()
+        self.assertEqual("Namespace(c='test', n=True)", str(args), "-c + -n command line arg fail")
 
 
 
